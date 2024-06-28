ed43cbab39fe09a952fc1ec5d13650206197aa3e
Aleksei Golubkov <nuriver@gmail.com>
2024-06-28 11:35:42 +0200

    feat: add basic project structure

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <p>My First React App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
