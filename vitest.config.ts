fad94c2cfd738a02f2a3f4b8231a40d7aba0d94c
Aleksei Golubkov <nuriver@gmail.com>
2024-07-25 19:39:24 +0200

    chore: update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: '/tests/setup.ts',
+    coverage: {
+      exclude: [
+        'src/main.tsx',
+        '.eslintrc.cjs',
+        'vite.config.ts',
+        'vitest.config.ts',
+      ],
+    },
   },
 });
