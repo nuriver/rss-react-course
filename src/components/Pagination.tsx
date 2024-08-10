cf3d2c1cb9e8965049465a2f627ca3815db2fc2d
Aleksei Golubkov <nuriver@gmail.com>
2024-08-10 17:56:18 +0200

    feat: refactor pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,15 +1,18 @@
 import createPagination from '../utilities/createPagination';
+import { useSearchParams } from '@remix-run/react';
 
 export default function Pagination({
   count,
 }: {
   count: number | undefined;
 }): JSX.Element | null {
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search') as string;
   const theme = 'light';
 
   if (!count || count <= 10) return null;
 
-  const paginationItems = createPagination(count);
+  const paginationItems = createPagination(count, search);
 
   return (
     <div
--- src/utilities/createPagination.tsx
+++ src/utilities/createPagination.tsx
@@ -1,12 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link } from '@remix-run/react';
 
-export default function createPagination(count: number): JSX.Element[] {
+export default function createPagination(
+  count: number,
+  search: string
+): JSX.Element[] {
   const numberOfPages = Math.ceil(count / 10);
   const paginationItems = [];
 
   for (let i = 1; i <= numberOfPages; i += 1) {
     const paginationItem = (
-      <Link className="pagination-item" key={i} to={`/search/${i}`}>
+      <Link
+        className="pagination-item"
+        key={i}
+        to={`/?search=${search}&page=${i}`}
+      >
         {i}
       </Link>
     );
